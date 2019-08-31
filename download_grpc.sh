86451d6b34ab372e40eff98c179c2ed2d2c5ef2b
Doug Roeper <douglas.roeper@gmail.com>
2019-08-31 15:58:12 -0700

    - Initial commit

---
+++ download_grpc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GRPC_URL="https://packages.grpc.io/archive/2019/07/0e7188ccbb139a45e0795ba2d10375e4d6672104-feea1eca-7cab-4f5e-abd5-843e979cc153/csharp/grpc_unity_package.1.23.0-dev.zip"
+GRPC_ARCHIVE="/tmp/grpc_unity.zip"
+INSTALL_DIR="./Assets/Plugins/grpc"
+
+rm -rf "$INSTALL_DIR"
+mkdir -p "INSTALL_DIR"
+
+wget "$GRPC_URL" -O "$GRPC_ARCHIVE"
+unzip "$GRPC_ARCHIVE" -d "$INSTALL_DIR"
+
